1cf1bef9543da51417981adaa9b2b4dd4650a83f
Jason Sanford <jasonsanford@gmail.com>
2013-12-23 17:59:50 -0700

    APIConfigTest

--- test_fulcrum.py
+++ test_fulcrum.py
@@ -3,9 +3,21 @@ import unittest
 import httpretty
 
 from fulcrum import Fulcrum
+from fulcrum.api import APIConfig
+from fulcrum.exceptions import NotFoundException, InvalidAPIVersionException
 
 key = 'e30e6ed0382322e7dbcb71ff365a5652f40eb602891b02a3b2ef67a4ad467b11'
 api_root = 'https://api.fulcrumapp.com/api/v2'
+
+
+class APIConfigTest(unittest.TestCase):
+    def test_invalid_version(self):
+        exc = None
+        try:
+            api_config = APIConfig(key='foo', version=99)
+        except Exception as exc:
+            pass
+        self.assertTrue(isinstance(exc, InvalidAPIVersionException))
 
 
 class FormTest(unittest.TestCase):
@@ -33,6 +45,16 @@ class FormTest(unittest.TestCase):
         self.assertEqual(form['id'], '5b656cd8-f3ef-43e9-8d22-84d015052778')
 
     @httpretty.activate
+    def test_find_not_found(self):
+        httpretty.register_uri(httpretty.GET, api_root + '/forms/lobster', status=404)
+        exc = None
+        try:
+            form = self.fulcrum_api.form.find('lobster')
+        except Exception as exc:
+            pass
+        self.assertTrue(isinstance(exc, NotFoundException))
+
+    @httpretty.activate
     def test_delete(self):
         httpretty.register_uri(httpretty.DELETE, api_root + '/forms/5b656cd8-f3ef-43e9-8d22-84d015052778', status=200)
-        form = self.fulcrum_api.form.delete('5b656cd8-f3ef-43e9-8d22-84d015052778')
+        self.fulcrum_api.form.delete('5b656cd8-f3ef-43e9-8d22-84d015052778')
