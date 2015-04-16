9fb4122bbfca6df96f00c7feb44375c7786cea9e
Jason Sanford <jasonsanford@gmail.com>
2015-04-15 23:35:11 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requires = ['requests>=2.1.0']
 
 setup(
     name='fulcrum',
-    version='1.4.0',
+    version='1.4.1',
     description='A python wrapper for the Fulcrum API',
     author='Jason Sanford',
     author_email='jasonsanford@gmail.com',
