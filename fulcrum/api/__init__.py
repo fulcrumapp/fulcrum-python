1b39b59d9a5dd6897211d89abaa15ced551cf232
Jason Sanford <jasonsanford@gmail.com>
2017-02-17 10:47:44 -0500

    Include version in User-Agent header

--- fulcrum/api/__init__.py
+++ fulcrum/api/__init__.py
@@ -2,6 +2,7 @@ import json
 
 import requests
 
+import fulcrum
 from fulcrum.exceptions import NotFoundException, UnauthorizedException, InternalServerErrorException
 
 
@@ -23,7 +24,10 @@ class BaseAPI(object):
 
     def call(self, method, path, data=None, extra_headers=None, url_params=None, json_content=True):
         full_path = self.api_config.api_root + path
-        headers = {'X-ApiToken': self.api_config.key}
+        headers = {
+            'X-ApiToken': self.api_config.key,
+            'User-Agent': 'Fulcrum Python API Client, Version {}'.format(fulcrum.__version__),
+        }
 
         if json_content:
             headers.update({'Accept': 'application/json'})
